dc7b9708289fd5705880fc90376cd68603b7b1aa
Nathan Ngo <nathanj238@gmail.com>
2021-07-23 01:18:46 +1000

    Added SacredGeometry cog

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
 from settings import settings
+from application.cogs.sacred_geometry_cog import SacredGeometry
 
 
 def init_bot():
     bot = commands.Bot(command_prefix=settings.COMMAND_PREFIX)
+
+    bot.add_cog(SacredGeometry(bot))
 
     # Register cogs here.
     return bot
