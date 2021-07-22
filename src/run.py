a6a3de6f12fda36416d186dfab4d3f70306e1831
Nathan Ngo <nathanj238@gmail.com>
2021-07-23 01:19:16 +1000

    Added dotenv loader

--- src/run.py
+++ src/run.py
@@ -1,6 +1,13 @@
 import application
+import dotenv
+import os
+
+
+dotenv.load_dotenv()
+_SECRET_TOKEN_NAME = "SECRET_TOKEN"
 
 
 if __name__ == "__main__":
+    print("Starting discord bot.")
     bot = application.init_bot()
-    bot.run()
+    bot.run(os.getenv(_SECRET_TOKEN_NAME))
